dd62169fee37056b1dc000cfaadeeb91fcdb13a4
Amier Shraideh <amiershr@gmail.com>
2024-07-12 01:53:00 +0200

    update if statment - string is empty

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,7 @@
 document.addEventListener('mouseup', () => {
   const selectedText = window.getSelection()?.toString().trim();
   console.log('Selected text:', selectedText);
-  if (selectedText) {
+  if (selectedText && selectedText.length > 0) {
     chrome.runtime.sendMessage({ action: 'saveSelection', data: selectedText }, (response) => {
       console.log('Response from background script:', response);
     });
